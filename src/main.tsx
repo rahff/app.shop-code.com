f5f856255910735248f9adfc655077f23746f3c1
Raphaël <74792991+rahff@users.noreply.github.com>
2025-07-01 16:57:13 +0200

    Setup AuthProvider with Cognito Configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 import { createRoot } from 'react-dom/client';
+import { AuthProvider } from 'react-oidc-context';
 import App from './App.tsx';
 import './index.css';
 
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.eu-west-3.amazonaws.com/eu-west-3_4sYZgkGwV",
+  client_id: "672adatjjqeolfdkpljt49g4qt",
+  redirect_uri: "https://app.shop-code.com/",
+  response_type: "code",
+  scope: "email openid profile",
+};
+
 createRoot(document.getElementById('root')!).render(
+  <AuthProvider {...cognitoAuthConfig}>
     <App />
+  </AuthProvider>
 );
