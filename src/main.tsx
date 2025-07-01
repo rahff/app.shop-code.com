621f7564625208fa9ee4c34836c517108da24653
rahff <raphaelandrey99@gmail.com>
2025-07-01 18:17:33 +0200

    add cognito oidc

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-west-3.amazonaws.com/eu-west-3_4sYZgkGwV",
   client_id: "672adatjjqeolfdkpljt49g4qt",
-  redirect_uri: "https://app.shop-code.com/",
+  redirect_uri: "http://localhost:5173/",
   response_type: "code",
   scope: "email openid profile",
 };
